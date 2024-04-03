0898b48e0a5667776dbc271e092b4ad79a5fc5a6
Seungmoky <xkwksdnjs@gmail.com>
2024-04-03 22:20:31 +0900

    chapter3: Basic Widgets start point

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,48 @@
 import 'package:flutter/material.dart';
-import 'recipe.dart';
-import 'recipe_detail.dart';
 
 void main() {
-  runApp(const RecipeApp());
+  // 1
+  runApp(const Yummy());
 }
 
-class RecipeApp extends StatelessWidget {
-  const RecipeApp({super.key});
+class Yummy extends StatelessWidget {
+  // TODO: Setup default theme
 
-  // This widget is the root of your application.
+  // 2
+  const Yummy({super.key});
+
+  // TODO: Add changeTheme above here
+
   @override
   Widget build(BuildContext context) {
-    // 2
-    final ThemeData theme = ThemeData();
-    // 3
+    const appTitle = 'Yummy';
+
+    // TODO: Setup default theme
+
+    //3
     return MaterialApp(
+      title: appTitle,
+      //debugShowCheckedModeBanner: false, // Uncomment to remove Debug banner
+
+      // TODO: Add theme
+
+      // TODO: Apply Home widget
+
       // 4
-      title: 'Recipe Calculator',
-      // 5
-      theme: theme.copyWith(
-        colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.grey,
-          secondary: Colors.black,
+      home: Scaffold(
+        appBar: AppBar(
+          // TODO: Add action buttons
+          elevation: 4.0,
+          title: const Text(
+            appTitle,
+            style: TextStyle(fontSize: 24.0),
+          ),
         ),
-      ),
-      // 6
-      home: const MyHomePage(title: 'Recipe Calculator'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // 1
-    return Scaffold(
-      // 2
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      // 3
-      body: SafeArea(
-        // TODO: Replace child: Container()
-        // 4
-        child: ListView.builder(
-          // 5
-          itemCount: Recipe.samples.length,
-          // 6
-          itemBuilder: (BuildContext context, int index) {
-            // 7
-            // TODO: Update to return Recipe card
-            return GestureDetector(
-              // 8
-              onTap: () {
-                // 9
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      // 10
-                      // TODO: Replace return with return RecipeDetail()
-                      return RecipeDetail(recipe: Recipe.samples[index]);
-                    },
-                  ),
-                );
-              },
-              // 11
-              child: buildRecipeCard(Recipe.samples[index]),
-            );
-          },
-        ),
-        // child: Container(),
-      ),
-    );
-  }
-
-  // TODO: Add buildRecipeCard() here
-  Widget buildRecipeCard(Recipe recipe) {
-    return Card(
-      // 1
-      elevation: 2.0,
-      // 2
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      // 3
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // 4
-        child: Column(
-          children: <Widget>[
-            Image(image: AssetImage(recipe.imageUrl)),
-            // 5
-            const SizedBox(
-              height: 14.0,
-            ),
-            // 6
-            Text(
-              recipe.label,
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Palatino',
-              ),
-            )
-          ],
+        body: const Center(
+          child: Text(
+            'You Hungry?😋',
+            style: TextStyle(fontSize: 30.0),
+          ),
         ),
       ),
     );
