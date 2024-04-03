bbc907afc46d326498f83d316f860b4cc5863bd4
Seungmoky <xkwksdnjs@gmail.com>
2024-04-03 19:58:52 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    final ThemeData theme = ThemeData();
+    // 3
+    return MaterialApp(
+      // 4
+      title: 'Recipe Calculator',
+      // 5
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.grey,
+          secondary: Colors.black,
+        ),
+      ),
+      // 6
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      // 2
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // 3
+      body: SafeArea(
+        // TODO: Replace child: Container()
+        // 4
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            // TODO: Update to return Recipe card
+            return Text(Recipe.samples[index].label);
+          },
+        ),
+        // child: Container(),
+      ),
+    );
+  }
+
+  // TODO: Add buildRecipeCard() here
+  Widget buildRecipeCard(Recipe recipe) {
+    // 1
+    return Card(
+      // 2
+      child: Column(
+        // 3
+        children: <Widget>[
+          // 4
+          Image(image: AssetImage(recipe.imageUrl)),
+          // 5
+          Text(recipe.label),
+        ],
+      ),
+    );
+  }
+}
